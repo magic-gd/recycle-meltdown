07af2f34308e77713af280a0359193d5aefd925d
Tim Greller <timlg07@gmail.com>
2021-05-01 00:31:28 +0200

    Tried to implement pushing boxes.

--- minigames/PaperMinigame/Player.gd
+++ minigames/PaperMinigame/Player.gd
@@ -6,16 +6,23 @@ var jumpForce = 720
 var gravity = 32
 
 func _physics_process(delta):
+	var direction = 0
+	
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -deltaV
+		direction = -1
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x = deltaV
+		direction = 1
 		
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = -jumpForce
 		
 	velocity.y += gravity
-	
-	velocity = move_and_slide(velocity, Vector2.UP)
-	
+	velocity = move_and_slide(velocity, Vector2.UP)	
 	velocity.x = lerp(velocity.x, 0, 0.1)
+	
+	for i in range(get_slide_count() - 1):
+		var collision = get_slide_collision(i)
+		if "carton" in collision.collider.name:
+			collision.collider.move(direction)
--- minigames/PaperMinigame/carton.gd
+++ minigames/PaperMinigame/carton.gd
@@ -1,11 +1,31 @@
 extends KinematicBody2D
 
-const FALLING_VELOCITY = 10
+const FALLING_VELOCITY = 100
+const PUSH_VELOCITY = 120
+const MOVE_DIST = 60
 var velocity = Vector2(0, 0)
+var gets_pushed = false
+var initial_pos
 
 func _ready():
 	velocity.y = FALLING_VELOCITY
 
 func _process(delta):
-	var collision = move_and_collide(velocity)
+	var collision = move_and_collide(velocity * delta)
 	
+	if gets_pushed:
+		var dist = diff(position.x, initial_pos.x)
+		if dist - MOVE_DIST < 0.1:
+			gets_pushed = false
+			velocity.x = 0
+	
+func move(direction):
+	if gets_pushed: return
+	
+	initial_pos = position
+	velocity.x = direction * PUSH_VELOCITY
+	gets_pushed = true
+	
+func diff(a, b):
+	if a > b: return a - b
+	return b - a

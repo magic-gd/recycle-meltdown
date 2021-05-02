a70ca3856a54b603dd77b26fadc9e74af9913fe8
magic-gd <63213481+magic-gd@users.noreply.github.com>
2021-05-02 10:01:59 +0200

    changed touchscreen buttons to TouchscreenButtons

--- UI/TouchInput.gd
+++ UI/TouchInput.gd
@@ -1,26 +1,37 @@
 extends Control
 
+func _ready():
+	_connect()
+
+func _connect():
+	$LeftButton.connect("pressed", self, "_on_LeftButton_pressed")
+	$LeftButton.connect("released", self, "_on_LeftButton_released")
+	$RightButton.connect("pressed", self, "_on_RightButton_pressed")
+	$RightButton.connect("released", self, "_on_RightButton_released")
+	$JumpButton.connect("pressed", self, "_on_JumpButton_pressed")
+	$JumpButton.connect("released", self, "_on_JumpButton_released")
 
 
-func _on_LeftButton_button_down():
+
+func _on_LeftButton_pressed():
 	Input.action_press("move_left")
 
 
-func _on_LeftButton_button_up():
+func _on_LeftButton_released():
 	Input.action_release("move_left")
 
 
-func _on_RightButton_button_down():
+func _on_RightButton_pressed():
 	Input.action_press("move_right")
 
 
-func _on_RightButton_button_up():
+func _on_RightButton_released():
 	Input.action_release("move_right")
 
 
-func _on_JumpButton_button_down():
+func _on_JumpButton_pressed():
 	Input.action_press("jump")
 
 
-func _on_JumpButton_button_up():
+func _on_JumpButton_released():
 	Input.action_release("jump")
